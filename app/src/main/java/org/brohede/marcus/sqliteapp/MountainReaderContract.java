825f55fe2389392bddd5481114300252ce379154
a97marbr <a97marbr@gmail.com>
2018-04-25 01:19:49 +0200

    Initial commit

---
+++ app/src/main/java/org/brohede/marcus/sqliteapp/MountainReaderContract.java
@@ -0,0 +1,20 @@
+package org.brohede.marcus.sqliteapp;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by marcus on 2018-04-25.
+ */
+
+public class MountainReaderContract {
+    // This class should contain your database schema.
+    // See: https://developer.android.com/training/data-storage/sqlite.html#DefineContract
+
+    private MountainReaderContract() {}
+
+    // Inner class that defines the Mountain table contents
+    public static class MountainEntry implements BaseColumns {
+        // TODO:
+    }
+
+}
