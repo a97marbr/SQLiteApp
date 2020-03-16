eba14fdeca4da6bb752f108a2f0e6a2efee7bbd8
a97marbr <a97marbr@gmail.com>
2020-03-16 14:37:11 +0100

    Re-added the original assignment comment.

--- app/src/main/java/com/example/sqliteapp/MainActivity.java
+++ app/src/main/java/com/example/sqliteapp/MainActivity.java
@@ -18,6 +18,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
+        TODO: Create an App that stores Mountain data in SQLite database
+        TODO: Schema for the database must include columns for all member variables in Mountain class
+              See: https://developer.android.com/training/data-storage/sqlite.html
+        TODO: The Main Activity must have a ListView that displays the names of all the Mountains
+              currently in the local SQLite database.
+        TODO: In the details activity an ImageView should display the img_url
+              See: https://developer.android.com/reference/android/widget/ImageView.html
+        TODO: The main activity must have an Options Menu with the following options:
+              * "Fetch mountains" - Which fetches mountains from the same Internet service as in
+                "Use JSON data over Internet" assignment. Re-use code.
+              * "Drop database" - Which drops the local SQLite database
+        TODO: All fields in the details activity should be EditText elements
+        TODO: The details activity must have a button "Update" that updates the current mountain
+              in the local SQLite database with the values from the EditText boxes.
+              See: https://developer.android.com/training/data-storage/sqlite.html
+        TODO: The details activity must have a button "Delete" that removes the
+              current mountain from the local SQLite database
+              See: https://developer.android.com/training/data-storage/sqlite.html
+        TODO: The SQLite database must not contain any duplicate mountain names
+     */
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
